74a4a5e7b1a20f7d02ff204299c818c3c2aaf6d3
codervipul775 <ydvvipul2005@gmail.com>
2025-03-18 23:40:53 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Code2, Linkedin, Twitter } from "lucide-react";
+import { Code2, Linkedin, Instagram } from "lucide-react";
 
 interface FooterProps {
   copyright: string;
