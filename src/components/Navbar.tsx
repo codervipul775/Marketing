58330eab1433040090cec7880b44f8fad3955f68
codervipul775 <ydvvipul2005@gmail.com>
2025-03-19 22:48:31 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Code2 } from 'lucide-react';
 import { Link, useLocation } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
@@ -13,7 +12,11 @@ const Navbar: React.FC = () => {
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
           <Link to="/" className="flex items-center gap-2">
-            <Code2 className="h-6 w-6 text-indigo-400" />
+            <img
+              src="https://media-hosting.imagekit.io//ec84fa2887bd4175/Influenz%20Logo.png?Expires=1837012577&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=Dn8rXLG2n2uHvuyVmag7cC4Po70RgnDStbMu2oweeJCahLVMIwgiBbNUvBPhaN3lIhZdDsOUX5jkqFUmB2hXyn6RzayHX7F~PP6QXJJHla5il831PLLQup1bm6WqQeEw6hazbwPvmZW~9oUWn-lVvZqfcFFH1eWTF5Y7BLhVSfD5aLTwrmQZ-ENgyy9c1g2T2UbsbMwEDNinRPgFTWJMxfKKp12OPB2cOZBUIM9JaRvNfhy6ZVp1eZVxVTI-DC9wXg099XpnqTC~im4j0EJXMfYABsCY74bkenoKQvwj3cCcgkvVSjyM6zVI~3twAuncMcUDoSSd4bG7bHOfz2HI0Q__"
+              alt="Influenz Logo"
+              className="h-8" // Adjust size as needed
+            />
             <span className="text-xl font-bold bg-gradient-to-r from-white to-indigo-300 text-transparent bg-clip-text">
               Influenz
             </span>
